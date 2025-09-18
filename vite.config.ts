f419c96bf882baa0013e6123aa0528e316be05ff
difasalt <difadev@gmail.com>
2025-09-18 09:51:35 +0700

    feat: [000] Remove fs cert

--- vite.config.ts
+++ vite.config.ts
@@ -8,10 +8,6 @@ export default defineConfig({
     exclude: ['lucide-react'],
   },
   server: {
-    https: {
-      key: fs.readFileSync('./cert/key.pem'),
-      cert: fs.readFileSync('./cert/cert.pem'),
-    },
     host: true, // bind ke semua network interface
     port: 5173, // port default Vite
     cors: true, // izinkan akses dari host lain
